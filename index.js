9713fb3b8eb176ed641e61a377a8b143e1e01626
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-07-10 10:55:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+{ products [
+    {
+      id: 0,
+      name: "FREE WIFI EMO 8.25.",
+      price: 749.99,
+      instock: 100,
+      description: "standard popsicle shape.",
+      category:"Skateboard",
+      imgSrc: "https://i.postimg.cc/QCZJxhKJ/154417-0-Free-Wifi-Emo825.webp",
+    },
+    {
+      id: 1,
+      name: "Will Marshall Pro Model.",
+      price: 599.99,
+      instock: 43,
+      description:
+        "Standard popsicle shape.",
+        category:"Skateboard",
+      imgSrc: "https://i.postimg.cc/BvNs4n82/149464-0-Alltimers-Will-NVA81.webp",
+    },
+    {
+      id: 2,
+      name: "Long Island LongBoard.",
+      price: 859.99,
+      instock: 10,
+      description:
+        "Twin-tip shape.",
+        category:"Longboard",
+      imgSrc: "https://i.postimg.cc/HLTHqyLp/143724-0-Long-Island-Endless3285834cm-Drop.webp",
+    },
+    {
+      id: 3,
+      name: "ELEMENT X STAR WARS MILLENIUM FALCON",
+      price: 549.99,
+      instock: 5,
+      description:
+        "Standard popsicle shape.",
+        category:"Skateboard",
+      imgSrc: "https://i.postimg.cc/x1vnpRnx/153376-0-Element-x-Star-Wars-Millenium-Falcon8.webp",
+    },
+    {
+      id: 4,
+      name: "ELEMENT X TIMBER FLOOD DRAGON",
+      price: 799.99,
+      instock: 4,
+      description:
+        "Standard popsicle shape.",
+        category:"Skateboard",
+      imgSrc: "https://i.postimg.cc/9MM2MHTK/162493-0-Element-x-Timber-Flood-Dragon85.webp",
+    },
+    {
+      id: 5,
+      name: "Globe Conical Cruiser.",
+      price: 999.99,
+      instock: 40,
+      description:
+        "Kicktail, wheelwells.",
+        category:"Longboard",
+      imgSrc: "https://i.postimg.cc/XJzbB1kJ/143996-0-Globe-The-All-Time35875905cm.webp",
+    }
+]
+}
